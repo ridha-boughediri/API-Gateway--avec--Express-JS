3014931a09841692b726c9b486f995b57630be03
ridha-boughediri <ridha.boughediri@laplateforme.io>
2024-05-04 16:59:47 +0200

    create route bogusapi

--- fakeapi/server.js
+++ fakeapi/server.js
@@ -1,16 +1,17 @@
-const express= require("express")
-const app= express()
-const PORT= 3001
+const express = require("express");
+const app = express();
+const PORT = 3001;
 
-app.use(express.json())
-app.get('/fakeapi', (req, res, next)=>{
-    res.send('bonjour de la fakemapi pour test')
-})
+app.use(express.json());
 
+app.get('/fakeapi', (req, res, next) => {
+    res.send('Bonjour de la fake API pour test');
+});
 
+app.get('/bogusapi', (req, res, next) => {
+    res.send('Bogus API dit shalom');
+});
 
-app.listen(PORT, ()=> {
-
-console.log('le server de l api fake est sur ce port ' + PORT)
-
-})
+app.listen(PORT, () => {
+    console.log('Le serveur de l\'API fake est sur le port ' + PORT);
+});
