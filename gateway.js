feb0d2e5b07603d2368f9477f2175b9f5e474122
ridha-boughediri <ridha.boughediri@laplateforme.io>
2024-05-04 17:28:26 +0200

    update

--- gateway.js
+++ gateway.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000; 
 const routes =require('./routes')
-
 app.use(express.json());
 app.use('/', routes)
 app.listen(PORT, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const registry = require("./registry.json");
 
-router.all('/:apiName', (req, res) => {
-    console.log(req.params.apiName);
-    axios.get("http://localhost:3001/" + req.params.apiName)
-        .then((response) => {
-            res.send(response.data); 
-        })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).send('Erreur lors de la requête à l\'API externe');
-        });
+router.all('/:apiName/:path', (req, res) => {
+    const apiName = req.params.apiName;
+    const path = req.params.path;
+
+    if (registry.services.hasOwnProperty(apiName)) {
+        const service = registry.services[apiName];
+        axios.get(`${service.url}/${path}`)
+            .then((response) => {
+                res.send(response.data); 
+            })
+            .catch((error) => {
+                console.error(error);
+                res.status(500).send('Erreur lors de la requête à l\'API externe');
+            });
+    } else {
+        res.status(404).send('Service non trouvé dans le registre');
+    }
 });
 
 module.exports = router;
