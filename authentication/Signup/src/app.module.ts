1da6651b9a6983faef52c6339b880ba5329552ab
Nouhayla <127697772+NouhaylaElfetehi@users.noreply.github.com>
2024-06-11 21:56:35 +0200

    CartService

--- authentication/Signup/src/app.module.ts
+++ authentication/Signup/src/app.module.ts
@@ -4,9 +4,10 @@ import {ConfigModule} from "@nestjs/config"
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from "./prisma/prisma.module";
 import { MailerModule } from './mailer/mailer.module';
+import { CartModule } from './Cart/cart.module';
 
 @Module({
 
-  imports: [ConfigModule.forRoot({isGlobal : true}),AuthModule,PrismaModule, MailerModule ],
+  imports: [ConfigModule.forRoot({isGlobal : true}),AuthModule,PrismaModule, MailerModule, CartModule ],
 })
 export class AppModule {}
